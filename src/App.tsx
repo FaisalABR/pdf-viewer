0e17b68fa2bcf2ba04bbc330bde581c94f463eb6
FaisalABR <faisalabubakar92@gmail.com>
2024-12-17 14:42:34 +0700

    chore: new implementation

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import "./App.css";
+import NewPDFViewer from "./components/pdf-viewer/NewPDFViewer";
 import PDFViewer from "./components/pdf-viewer/PDFViewer";
 
 function App() {
 	return (
 		<>
-			<PDFViewer fileUrl="https://raw.githubusercontent.com/mozilla/pdf.js/ba2edeae/web/compressed.tracemonkey-pldi-09.pdf" />
+			{/* <PDFViewer fileUrl="https://raw.githubusercontent.com/mozilla/pdf.js/ba2edeae/web/compressed.tracemonkey-pldi-09.pdf" /> */}
+			<NewPDFViewer fileUrl="https://raw.githubusercontent.com/mozilla/pdf.js/ba2edeae/web/compressed.tracemonkey-pldi-09.pdf" />
 		</>
 	);
 }
